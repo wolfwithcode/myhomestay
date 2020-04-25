3bc1374a39433963a8fd96446ce6f097f37e76f9
wolfwithcode <wolfwithcode@gmail.com>
2020-04-25 17:56:33 +0700

    Implement header of globamatics app

---
+++ src/main-page/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
