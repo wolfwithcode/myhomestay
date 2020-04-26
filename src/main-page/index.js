ad6c9f037647f4660330d5f4508b76e82e24a8eb
wolfwithcode <wolfwithcode@gmail.com>
2020-04-26 09:54:13 +0700

    Add filter function

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
+import FeaturedHouse from './featured-house'
+import HouseFilter from './house-filter';
 
-function App() {
-  return (
-    <div className="container">
-      <Header subtitle="Providing tiny house all over the world"/>
-    </div>
-  );
+class App extends Component {
+
+  state = {}
+
+  componentDidMount(){
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+      fetch('/houses.json')
+      .then(rsp => rsp.json())
+      .then(allHouses => {
+        this.allHouses = allHouses;
+        this.determineFeatureHouse();
+        this.determineUniqueCountries();
+      })
+    }
+
+  determineFeatureHouse = () => {
+    if(this.allHouses){
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    };
+  }
+
+  determineUniqueCountries = () => {
+    const countries = this.allHouses
+          ? Array.from(new Set(this.allHouses.map(h => h.country)))
+          : [];
+    countries.unshift(null);
+    this.setState({ countries });
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <Header subtitle="Providing tiny house all over the world"/>
+        <HouseFilter  countries={this.state.countries} />
+        <FeaturedHouse house={this.state.featuredHouse} />
+      </div>
+    );
+  }
 }
 
 export default App;
